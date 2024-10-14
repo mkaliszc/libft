1f3f39c2295c0f299461f9e9263912c7b359b8ba
mkaliszc <milan.kaliszczak@gmail.com>
2024-10-14 11:43:09 +0200

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 23:13:36 by mkaliszc          #+#    #+#             */
+/*   Updated: 2024/10/13 01:18:27 by mkaliszc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nbr;
+	int	sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\v')
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr * sign);
+}
+/*
+int main()
+{
+    //(void)argc;
+    printf("%d", ft_atoi("     -23a"));
+    return(0);
+}*/
