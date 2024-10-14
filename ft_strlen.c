53f1087e421877aebbe90984240789073fba0e09
mkaliszc <milan.kaliszczak@gmail.com>
2024-10-14 11:30:02 +0200

    Add files via upload

---
+++ ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 18:40:48 by mkaliszc          #+#    #+#             */
+/*   Updated: 2024/10/06 18:40:48 by mkaliszc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+# include<libft.h>
+
+size_t ft_strlen(const char *s)
+{
+    size_t i;
+
+    i = 0;
+    while (s[i] != '\0')
+        i += 1;
+    return (i);
+}
+/*
+int main()
+{
+    char *c = "Hello";
+    printf( "%d" , ft_strlen(c));
+    return(0);
+} */
---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 09:36:50 by mkaliszc          #+#    #+#             */
+/*   Updated: 2024/10/03 09:36:50 by mkaliszc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+
+int ft_isalpha(int arg);
+int ft_isdigit(int arg);
+int ft_isalnum(int arg);
+size_t ft_strlen(const char *s);
+int ft_isascii (int c);
+int ft_isprint(int c);
+
+
+#endif
