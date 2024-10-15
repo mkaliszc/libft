78c1622bcc67f34e295af27280e355c9990063d3
mkaliszc <milan.kaliszczak@gmail.com.com>
2024-10-15 17:08:07 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,30 +6,51 @@
 /*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 18:01:30 by mkaliszc          #+#    #+#             */
-/*   Updated: 2024/10/14 22:21:03 by mkaliszc         ###   ########.fr       */
+/*   Updated: 2024/10/15 17:06:39 by mkaliszc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include "libft.h"
+#include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	unsigned char *cast1;
-	const unsigned char *cast2;
+	unsigned char *cast_dest;
+	const unsigned char *cast__src;
 
-	cast1 = (unsigned char *)dest;
-	cast2 = (const unsigned char *)src;
-	if (cast1 < cast2)
+	cast_dest = (unsigned char *)dest;
+	cast__src = (const unsigned char *)src;
+	if (cast_dest < cast__src)
 	{
+		i= 0;
 		while (i < n)
 		{
-			cast1[i] = cast2[i];
+			cast_dest[i] = cast__src[i];
 			i++;
 		}
 	}
-	else if (cast2 < cast1)
+	else if (cast__src < cast_dest)
 	{
-		
+		i= n;
+		while (0 < i)
+		{
+			cast_dest[i] = cast__src[i];
+			i--;
+		}
 	}
-} */
+	return (dest);
+}
+
+int main()
+{
+	char test[]= "Hello, World!";
+
+	//ft_memmove(test + 7, test, 6);
+	//printf("test 1 :%p\n", test);
+	memmove(test + 7, test, 6);
+
+	printf("%s\n", test);
+	//printf("%p\n", memmove(test + 7, test, 6));
+	return(0); 
+}
